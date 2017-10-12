eff879c3ba36edb5fce107079f29ee6ee9f103f2
HuangHaoDong <534634672@qq.com>
2017-10-12 10:49:31 +0800

    退出登录下来点

--- src/modules/Me/page/index.js
+++ src/modules/Me/page/index.js
@@ -58,7 +58,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#eee',
-
+        justifyContent:'center',
+        alignItems:'center'
     }
 });
 
