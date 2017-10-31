c2cb674d46dcf3b65c74e91ebf7bd7e3d22ad412
castiel <252182398@qq.com>
2017-10-31 12:02:36 +0800

    正是服务器

--- src/Core/Networking/NetworkConfig.js
+++ src/Core/Networking/NetworkConfig.js
@@ -1,8 +1,8 @@
 /**
  * Created by apple on 2017/5/15.
  */
-// export const baseURL = "http://imapi.d1.natapp.cc/"
- export const baseURL = "http://192.168.0.2:7077/"
+export const baseURL = "http://imapi.d1.natapp.cc/"
+//  export const baseURL = "http://192.168.0.2:7077/"
 //export const baseURL = "http://192.168.0.143:8888/"
  // export const baseURL = "192.168.0.143:8112"
 // export const baseURL = "http://maskhao.cn:8080"
