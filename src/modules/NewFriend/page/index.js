28b4c44c7727987437284b19335abe71432542f5
zhangtong <402931710@qq.com>
2017-11-08 16:58:32 +0800

    添加好友申请菊花

--- src/modules/NewFriend/page/index.js
+++ src/modules/NewFriend/page/index.js
@@ -68,7 +68,9 @@ class NewFriend extends ContainerComponent {
 
     agreeApply = (index,data)=>{
         let {key,send} = data;
+        this.showLoading();
         this.fetchData('POST','Member/AcceptFriend',function (result) {
+            currentObj.hideLoading();
             if(result.success){
                 // let addMessage = addAddFriendMessage({comment:currentObj.props.accountName,key},currentObj.props.accountId,send);
                 // im.addMessage(addMessage,function(){
@@ -164,6 +166,7 @@ class NewFriend extends ContainerComponent {
         )
     }
     render() {
+        let Loading = this.Loading;
         return (
             <View style={styles.container}>
                 <MyNavigationBar
@@ -188,6 +191,7 @@ class NewFriend extends ContainerComponent {
                     </ListView>
 
                 </ScrollView>
+                <Loading ref = { loading => this.loading = loading}/>
             </View>
             )
             
