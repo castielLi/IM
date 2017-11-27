520a317f26805c0ad62ac8986214798ca458f4c6
castiel <252182398@qq.com>
2017-11-27 16:09:40 +0800

    refactor

---
+++ src/Controller/ApiBridge/apiConfig.js
@@ -0,0 +1,9 @@
+/**
+ * Created by apple on 2017/11/27.
+ */
+
+export const API_Login = "Member/Login";
+
+export const API_LoginByToken = "Member/LoginByToken";
+
+export const API_GetContactList = "Member/GetContactList";
