95a61aca221e999cf08092d5d94f7b6fb4836105
castiel <252182398@qq.com>
2017-12-05 17:50:20 +0800

    refactor

---
+++ src/Logic/Chat/methods/SqliteMessageToMessageDto.js
@@ -0,0 +1,10 @@
+/**
+ * Created by apple on 2017/12/5.
+ */
+
+import MessageDto from '../dto/MessageDto'
+
+export function SqliteMessageToMessageDto(message){
+   let messageDto = new MessageDto();
+   // messageDto.sendTime = message.Data.
+}
