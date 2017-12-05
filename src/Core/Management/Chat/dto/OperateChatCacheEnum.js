4e522e2cb1ec63c7b473d0714ee7c00cc52d40c8
HuangHaoDong <534634672@qq.com>
2017-12-05 11:31:26 +0800

    update

--- src/Core/Management/Chat/dto/OperateChatCacheEnum.js
+++ src/Core/Management/Chat/dto/OperateChatCacheEnum.js
@@ -1,7 +1,8 @@
 
 
 export default OperateChatCacheEnum = {
-    Send : 'send',
-    Receive :'receive',
-    Unread:'unread'
+    Send : 'send',//发送消息
+    Receive :'receive',//收到消息
+    Unread:'unread',//将某个item的未读消息清零
+    Message:'message'//修改消息
 }
