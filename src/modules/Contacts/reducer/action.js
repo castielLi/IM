0247199f5e5ef824ba25c04d806d4b1645da9975
zhangtong <402931710@qq.com>
2018-01-06 17:40:03 +0800

    fix

---
+++ src/modules/Contacts/reducer/action.js
@@ -0,0 +1,15 @@
+export function showUnReadMark(){
+    return {
+        type: 'SHOW_UNREAD_MARK',
+    };
+
+}
+
+export function hideUnReadMark(){
+    return {
+        type: 'HIDE_UNREAD_MARK',
+    };
+
+}
+
+
