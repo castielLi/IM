dc81d0f8af780b5eff83653aa43d7c538dc893c8
zhangtong <402931710@qq.com>
2018-01-11 10:56:26 +0800

    fix

--- src/modules/ChooseClient/page/index.js
+++ src/modules/ChooseClient/page/index.js
@@ -266,7 +266,7 @@ class ChooseClient extends AppComponent {
             currentObj.showLoading();
 
 			//参数：发起人id,群id,添加成员昵称,添加成员id字符串(xx,xx,xx),
-			userController.addGroupMember(currentAccount.Account,this.props.groupId,Nicks,accounts,(result)=>{
+			userController.addGroupMember(this.props.groupId,accounts,(result)=>{
                 currentObj.hideLoading();
                 if(result.Result == 1){
                     let routes = currentObj.props.navigator.getCurrentRoutes();
@@ -297,10 +297,9 @@ class ChooseClient extends AppComponent {
 			}
             currentObj.showLoading();
 			let groupName = currentAccount.Nickname + "发起的群聊";
-			userController.createGroup(currentAccount.Account,groupName,Nicks,accounts,(result)=>{
+			userController.createGroup(groupName,accounts,(result)=>{
                 currentObj.hideLoading();
 				if(result.Result == 1){
-
                     currentObj.route.push(currentObj.props,{key:'ChatDetail',routeId:'ChatDetail',params:{client:result.Data,type:"group",Nick:groupName}});
 
 				}else{
