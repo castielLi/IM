e472b27ba70c6a28483dba5fd5feb08bae6ce144
castiel <252182398@qq.com>
2018-03-05 10:44:20 +0800

    changes

---
+++ src/TSCore/WebApiManagement/UserManagement/Dtos/UserSettingDto.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Hsu. on 2018/3/2.
+ */
+export default class UserSettingDto {
+    constructor() {
+        this.BlackList = false;
+        this.ScanFriends = false;
+        this.ReScanFriends = false;
+        this.SpecialFriend = false;
+    }
+}
+//# sourceMappingURL=UserSettingDto.js.map
