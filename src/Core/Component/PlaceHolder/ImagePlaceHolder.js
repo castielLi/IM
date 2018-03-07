b97fdb28cd2dca051184e437d1b54840148ca5d7
zhangtong <402931710@qq.com>
2018-03-07 13:56:28 +0800

    fix

--- src/Core/Component/PlaceHolder/ImagePlaceHolder.js
+++ src/Core/Component/PlaceHolder/ImagePlaceHolder.js
@@ -13,7 +13,6 @@ import {
     checkDeviceHeight,
     checkDeviceWidth
 } from '../../Helper/UIAdapter';
-
 export default class ImagePlaceHolder extends Component {
 
     constructor(props) {
@@ -21,10 +20,16 @@ export default class ImagePlaceHolder extends Component {
         this.state = {
             show:true
         }
-        this.newUrl = true;
+        // this.newUrl = true;
     }
 
     shouldComponentUpdate(nextProps, nextState) {
+        // alert(JSON.stringify(nextProps)+JSON.stringify(nextState))
+        // alert(nextProps.imageUrl+'****'+this.props.imageUrl)
+        // if(nextProps.imageUrl != this.props.imageUrl){
+            //alert(nextProps.imageUrl+'****'+this.props.imageUrl)
+            // this.newUrl = true;
+        // }
         if(nextProps.imageUrl != this.props.imageUrl || nextState.show != this.state.show){
             return true;
         }else{
@@ -32,34 +37,34 @@ export default class ImagePlaceHolder extends Component {
         }
     }
 
-    componentWillReceiveProps() {
-        this.newUrl = true;
-    }
+    // componentWillReceiveProps() {
+    //      this.newUrl = true;
+    // }
 
     render() {
         let _style = this.props.style ? this.props.style : this.styles.avatar;
         // alert(this.props.imageUrl + "?v=" + new Date().getTime()+this.state.show)
-        let _default = require('./resource/avator.jpg');
+        // let _default = require('./resource/avator.jpg');
         return (
             <View>
-                {/*<Image source={{uri:  Platform.OS==='ios'?this.props.imageUrl:this.props.imageUrl + "?v=" + new Date().getTime()}} style={ this.state.show?_style:[styles.hide]}*/}
-                       {/*onError={()=>{*/}
-                           {/*this.setState({*/}
-                               {/*show:false*/}
-                           {/*})*/}
-                       {/*}}*/}
-                {/*/>*/}
-                {/*<Image source={require('./resource/avator.jpg')} style={this.state.show?[styles.hide]:_style}/>*/}
-
-                <Image source={(this.state.show || this.newUrl) ? {uri:this.props.imageUrl} : _default}
-                       style={_style}
+                <Image source={{uri:this.props.imageUrl }} style={_style}
                        onError={()=>{
                            this.setState({
                                show:false
-                           });
-                           this.newUrl = false;
+                           })
                        }}
                 />
+                {this.state.show ? null : <Image source={require('./resource/avator.jpg')} style={[_style,{position:'absolute'}]}/>}
+
+                {/*<Image source={(this.state.show || this.newUrl) ? {uri:this.props.imageUrl} : _default}*/}
+                       {/*style={_style}*/}
+                       {/*onError={()=>{*/}
+                           {/*this.newUrl = false;*/}
+                           {/*this.setState({*/}
+                               {/*show:false*/}
+                           {/*});*/}
+                       {/*}}*/}
+                {/*/>*/}
             </View>
         );
     }
