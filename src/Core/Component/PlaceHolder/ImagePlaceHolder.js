d3f29cd1c2abc1d6114af8b292e1d6ef48fdff5a
castiel <252182398@qq.com>
2018-04-03 10:47:44 +0800

    changes
    changes

--- src/Core/Component/PlaceHolder/ImagePlaceHolder.js
+++ src/Core/Component/PlaceHolder/ImagePlaceHolder.js
@@ -17,69 +17,35 @@ export default class ImagePlaceHolder extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            show:false
-        };
-        this.success = true;
     }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        // alert(JSON.stringify(nextProps)+JSON.stringify(nextState))
-        // alert(nextProps.imageUrl+'****'+this.props.imageUrl)
-        // if(nextProps.imageUrl != this.props.imageUrl){
-            //alert(nextProps.imageUrl+'****'+this.props.imageUrl)
-            // this.newUrl = true;
-        // }
-        if(nextProps.imageUrl != this.props.imageUrl || nextState.show != this.state.show){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    // componentWillReceiveProps() {
-    //      this.newUrl = true;
-    // }
 
     render() {
         let _style = this.props.style ? this.props.style : styles.avatar;
-        // alert(this.props.imageUrl + "?v=" + new Date().getTime()+this.state.show)
-        // let _default = require('./resource/avator.jpg');
         let url = (typeof this.props.imageUrl === 'string') ? {uri:this.props.imageUrl } : this.props.imageUrl;
         return (
             <View>
-                {this.state.show ? null : <Image source={require('./resource/avator.jpg')} style={[_style,{position:'absolute'}]}/>}
-                <Image source={url} style={_style}
+                <Image ref={ image => this.image = image}
+                    source={url} style={_style}
                        onError={()=>{
-                           {/*this.success = false;*/}
-                           if(this.state.show){
-                               this.setState({
-                                   show:false
-                               })
-                           }
+                           this.image.setNativeProps({
+                               style:styles.hide
+                           })
                        }}
                        onLoad={()=>{
-                           {/*this.success = true;*/}
-                           if(!this.state.show){
-                               this.setState({
-                                   show:true
-                               })
-                           }
+                           this.defaultImage.setNativeProps({
+                               style:styles.hide
+                           })
                        }}
                 />
-                {/*<Image source={(this.state.show || this.newUrl) ? {uri:this.props.imageUrl} : _default}*/}
-                       {/*style={_style}*/}
-                       {/*onError={()=>{*/}
-                           {/*this.newUrl = false;*/}
-                           {/*this.setState({*/}
-                               {/*show:false*/}
-                           {/*});*/}
-                       {/*}}*/}
-                {/*/>*/}
+                <Image
+                    ref={ defaultImage => this.defaultImage = defaultImage}
+                    source={require('./resource/avator.jpg')} style={[_style]}/>
+
             </View>
         );
     }
 }
+
 const styles = StyleSheet.create({
     hide:{
         display:"none"
