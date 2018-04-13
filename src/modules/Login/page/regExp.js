4cbbdee869e778a446f837362b0141a3c534e850
zhangtong <402931710@qq.com>
2018-04-13 14:17:19 +0800

    fix

--- src/modules/Login/page/regExp.js
+++ src/modules/Login/page/regExp.js
@@ -2,7 +2,7 @@ export default checkReg = (type,value)=>{//type:1 手机登录  2:邮箱登录
 	switch(type){
 		case 1:
 			if(!(/^1[34578]\d{9}$/.test(value))){
-				alert('账号必须是手机');
+				// alert('账号必须是手机');
 				return false;
 			}else{
 				return true;
@@ -10,7 +10,7 @@ export default checkReg = (type,value)=>{//type:1 手机登录  2:邮箱登录
 		break;
 		case 2:
 			if (!(/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(value))) {
-				alert('账号必须是邮箱');
+				// alert('账号必须是邮箱');
 			}
 		break;
 		
